f550d1eccb49295e7c3b2cf36328148b7df16494
Muhammad Suleman <iam.suleman.m@gmail.com>
2024-08-22 02:36:02 +0500

    FP-header
     - Created header for all pages

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-content">
+                <h2>Order Your Favourit food here</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam consequuntur labore, maiores eius nisi debitis unde et architecto explicabo exercitationem aliquam est ad ea eos maxime natus. Explicabo, omnis ad.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
